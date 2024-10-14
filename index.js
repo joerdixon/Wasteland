ef8e41d185cfae64bf244474e11c1a3402d89847
JoeDixon <joseph35dixon@gmail.com>
2024-10-13 21:21:55 -0700

    pseudo

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
-// DOM Selectors
+// Wasteland Crates
+
+// Pseudocode
+
+// Determine Rarity
+// Determine Item Type
+// Pick Item from Designated Pool (eg. Uncommon Armor)
+    // If weapon
+        // If broken, return broken weapon with random attachment.
+    // Determine attachment slots
+    // determineAttachments
+        // Determine attachment compatability (Melee/Ranged)
+        // Determine attachment rarity (Normal, Special)
+        // Add attachments
+
+// Print info
+
+//DOM
+
+let rollDisplay = document.getElementById("roll-display");
+let testDisplay = document.getElementById("testbox");
+
+
+const commons = [
+
+]
+
+// Determine Rarity
+
+
+
+// Possible Items
 
 // Initiates the opening of the crate.
 function unsealCrate() {
-    console.log("test");
+    testDisplay.innerHTML = Dice(20);
 }
+
+// Dice Roller w/ param for sides.
+function Dice(sides) {
+    return Math.floor(Math.random() * sides) + 1;
+}
+
